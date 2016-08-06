d8dcdd2d4b938a2bf9251f95cc1a23316ba6f0c6
Ronn Ross <ronn.ross@gmail.com>
2016-08-05 22:28:29 -0400

    Back and forth buttons working on presentation view

--- src/components/slide-controls.js
+++ src/components/slide-controls.js
@@ -1,8 +1,12 @@
 const html = require('choo/html')
 
-module.exports = (state, prev, send) => html`
+module.exports = (currentSlide) => {
+
+  return html`
   <div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
-    <a href="/edit" class="btn-floating yellow"><i class="material-icons">skip_previous</i></a>
-    <a href="/presentation" class="btn-floating yellow"><i class="material-icons">skip_next</i></a>
+    <a href="/presentation/${parseInt(currentSlide) - 1}" class="btn-floating green"><i class="material-icons">skip_previous</i></a>
+    <a href="/edit" class="btn-floating blue"><i class="material-icons">mode_edit</i></a>
+    <a href="/presentation/${parseInt(currentSlide) + 1}" class="btn-floating green"><i class="material-icons">skip_next</i></a>
   </div>
 `
+}
--- src/views/presentation.js
+++ src/views/presentation.js
@@ -3,4 +3,12 @@ const menu = require('../components/menu')
 const slideControl = require('../components/slide-controls')
 const slide = require('../components/slide')
 
-module.exports = (state, prev, send) => html`<main>${slide(state.rd.slides[state.params.id])}</main>`
+module.exports = (state, prev, send) => {
+  let currentSlide = state.params.id || 0
+
+  return html`
+  <main>
+    ${slide(state.rd.slides[currentSlide])}
+    ${slideControl(currentSlide)}
+  </main>
+`}
