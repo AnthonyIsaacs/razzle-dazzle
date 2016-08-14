7afbab75805b626d7db922ff873cbe37c0c3e699
Ronn Ross <ronn.ross@gmail.com>
2016-08-14 15:14:26 -0400

    Attempting to get hightlightjs to work in slides

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const sf = require('sheetify')
 const choo = require('choo')
+const highlight = require('highlight.js')
+const js = require('../node_modules/highlight.js/lib/languages/javascript.js')
 
 sf('normalize.css', { global: true })
 sf('./styles/material-icons.css', { global: true })
 sf('../node_modules/materialize-css/dist/css/materialize.css', { global: true })
 sf('./styles/main.css', { global: true })
+sf('../node_modules/highlight.js/styles/zenburn.css')
 
 const app = choo()
 if (process.env.NODE_ENV !== 'production') {
